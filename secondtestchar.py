d0225ddc0532c100cbb3b32cc2589dd8ac12b205
Taryn Atchley <taryn@Talyn.local>
2018-09-15 23:31:50 -0500

    Removed unnecessary test files used for troubleshooting.

--- secondtestchar.py
+++
@@ -1,6 +0,0 @@
-import diceFunctions as dF
-import characterChoices as cC
-
-from races import *
-from classes import *
-from backgrounds import *
--- ulaara.py
+++
@@ -1,4 +0,0 @@
-from races import *
-from classes import *
-
-scores = [18 14 14 13 13 12]
